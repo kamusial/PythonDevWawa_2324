1019499f865c30743cd5827f05bb5ae2a4f0551b
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-10-21 11:24:03 +0200

    1

---
+++ Zjazd1_21_10_23/1_Sobota_razem/1_print_zmienne.py
@@ -0,0 +1,28 @@
+print('Dzien dobry')   # pojedyncze sudzysłowy
+print("Dzien dobry")   # podwójne cudzysłowy
+print("She's great")
+
+a = 5         # int - integer - zmienna całkowita
+b = 6.5       # float - zmiennoprzecinowa
+c = 'piesek'  # str - string - napis
+
+print('a')    # wypisz litterkę 'a'
+print(a)      # wypisz zmienną a
+print(a+b)    # napisz wynik równiania a + b
+print(3*a/b)
+# print(a + c)    # nie można dodać liczby do napisu
+print(a * c)     # mozna mnożyc napis przez liczbę całkowitą
+
+d = a + b + 4
+print(d)
+
+print(a * (c + 'dom'))     #napisz 5 razy "piesekdom"
+print('\n')
+liczba1 = 3    # int - zmienna całkowita
+liczba2 = '3'  # str - napis
+
+print(2 * liczba1)
+print(2 * liczba2)
+
+print(type(liczba1))   #wypisz typ zmiennej
+print(type(liczba2))
