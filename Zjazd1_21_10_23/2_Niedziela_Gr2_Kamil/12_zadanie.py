cc194e2a20a952afc643b6221eb246cb8fc27642
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-10-22 15:02:13 +0200

    1

--- Zjazd1_21_10_23/2_Niedziela_Gr2_Kamil/12_zadanie.py
+++ Zjazd1_21_10_23/2_Niedziela_Gr2_Kamil/12_zadanie.py
@@ -8,7 +8,24 @@
 
 data = []
 while True:
-    person = input('Wprowadź dane: Imie, nazwisko, mail - oddziel spacją').split()
+    person = input('Wprowadź dane: Imie, nazwisko, mail - oddziel spacją ').split()
     data.append(person)
-    decission = input('Czy chcesz dodać kolejną osobę? T/N')
-    if decission
+    decission = input('Czy chcesz dodać kolejną osobę? T/N ')
+    if decission.upper() != 'T':
+        break
+no_of_women = 0
+no_of_gmail = 0
+for person in data:
+    if person[0][-1] == 'a':
+        no_of_women += 1
+    if '@gmail.' in person[2]:
+        no_of_gmail += 1
+print(f' Liczba kobiet: {no_of_women}, a liczba uzytkowników gmail: {no_of_gmail}')
+print(f'Lista osób: {data}')
+#print(data[0][2][7:10])   # wyciągnicie kawałka maila
+# usuwanie niepasujących imion
+names = ['Kamil', 'Marek', 'Basia', 'Kasia', 'Gustaw', 'Mietek']
+for person in data:
+    if person[0] not in names:
+        data.remove(person)
+print(f'Końcowa lista wygląda tak: {data}\n, jest {len(data)} osób')
---
+++ Zjazd1_21_10_23/2_Niedziela_Gr2_Kamil/13_domowe.py
@@ -0,0 +1,7 @@
+# Zadanie domowe:
+# dana jest lista użytkowników
+# User_list = ['majki', 'Kamil001', 'Rafcio', 'Betty']
+# - program pyta o wprowadzenie nowego użytkownika
+# - jeśli nazwa użytkownika jest zajęta, program prosi o ponowne wprowadzenie nazwy bądź proponuje swoją podobną nazwę
+# - po wprowadzeniu uzytkownika, program 2 razy prosi o hasło
+# - po dwukrotnie wprowadzonym takim samym haśle, uzytkownik zostaje dodany do listy użytkowników, a jego hasło jest zapisane w drugiej liście
