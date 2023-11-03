d871c50b35eccd644d90fa978f17d59081b27d7f
musiakam <kamil.musial@tietoevry.com>
2023-11-03 21:09:44 +0100

    1

--- 2_Niedziela_Gr1_Natalia/zadanie_1.py
+++
@@ -1,6 +0,0 @@
-# Program sprawdza, ile masz lat.
-# jesli masz > 13 lat, to dostrcz zgodę rodzica
-# jesli masz > 8 lat, to dostarcz zgodę psychologa
-# jesli masz > 18 lat, to jest OK, weryfikacja poprawna
-# jeśli jestes za młody, to zweryfikuj się jeszcze raz.
-
