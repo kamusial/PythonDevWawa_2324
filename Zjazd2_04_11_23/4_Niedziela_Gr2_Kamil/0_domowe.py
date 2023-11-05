a3d28d0ce7c3f4e8958afa370ab3d2fc7d4ee30b
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-11-05 10:01:05 +0100

    1

--- Zjazd2_04_11_23/4_Niedziela_Gr2_Kamil/0_domowe.py
+++ Zjazd2_04_11_23/4_Niedziela_Gr2_Kamil/0_domowe.py
@@ -4,32 +4,13 @@
 # po wprowadzeniu uzytkownika, program 2 razy prosi o hasło
 # po dwukrotnie wprowadzonym takim samym haśle, uzytkownikzostaje dodany do listy użytkowników, a jego hasło jest zapisane w drugiej liście
 
-def user_add(username):
-    user_list.append(username)
-    print(f'uzytkownik {username} dodany do listy uzytkowników')
+# import funkcje_do_domowe
+# funkcje_do_domowe.is_username_available()
+
+from funkcje_do_domowe import *
 
 
-def is_username_available(username):
-    if username not in user_list:
-        print(f'Nazwa {username} dozwolona')
-        return True
-    else:
-        print(f'Nazwa {username} NIEdozwolona')
-        return False
-
-
-def suggest_username(username):
-    return username + '1'
-
-
-def find_available_name(username):
-    while not is_username_available(suggest_username(username)):
-        username = suggest_username(username)
-    username = suggest_username(username)
-    print(f'Dostępna nazwa {username}')
-    return username
-
-user_list = ['majki', 'Kamil', 'Kamil1', 'Kamil11', 'Kamil111', 'Kamil001', 'Rafcio', 'Betty']
+#user_list = ['majki', 'Kamil', 'Kamil1', 'Kamil11', 'Kamil111', 'Kamil001', 'Rafcio', 'Betty']
 
 
 username = input('Podaj nazwe uzytkownika: ')
