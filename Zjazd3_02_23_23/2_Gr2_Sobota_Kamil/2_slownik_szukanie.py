7c0064823f078f4984ead20ee743e257ba54316e
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-02 10:05:20 +0100

    1

---
+++ Zjazd3_02_23_23/2_Gr2_Sobota_Kamil/2_slownik_szukanie.py
@@ -0,0 +1,8 @@
+browser_speed = {'Chrome': 100, 'Opera': 23, 'Firefox': 76, 'Safari': 58}
+
+print(list(browser_speed.keys()))
+print(list(browser_speed.values()))
+print(list(browser_speed.items()))
+
+print(list(browser_speed.keys())[list(browser_speed.values()).index(76)])
+
