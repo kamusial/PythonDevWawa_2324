db7b1d70a65439ca89749d04228c4a335382fe11
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-03 14:29:34 +0100

    1

---
+++ Zjazd3_02_23_23/4_Gr2_niedziela_Kamil/7_OOP.py
@@ -0,0 +1,12 @@
+class Auto:
+    def __init__(self):
+        self.kolor = 'red'
+        self.ilosc_paliwa = 10
+        self.kondycja = 5
+        self.tryb_ekonomiczny = False
+        self.spalanie_na_100 = 14
+
+
+moje_auto = Auto()
+auto_sasiada = Auto()
+
