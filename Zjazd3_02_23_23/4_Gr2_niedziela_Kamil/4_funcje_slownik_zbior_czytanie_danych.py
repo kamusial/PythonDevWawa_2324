437c5c54fab342cbabb74acb04fa257e9c9adc2f
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-03 11:51:21 +0100

    1

--- Zjazd3_02_23_23/4_Gr2_niedziela_Kamil/4_funcje_slownik_zbior_czytanie_danych.py
+++ Zjazd3_02_23_23/4_Gr2_niedziela_Kamil/4_funcje_slownik_zbior_czytanie_danych.py
@@ -9,7 +9,7 @@ parser.add_argument('-m','--mode', help='Tryb w, r, x',
 parser.add_argument('-now', '--number_of_words', type=int, default=0, required=False)
 parser.add_argument('-uw', '--unique_words', type=int, default=0, required=False)
 parser.add_argument('-d', '--duplicates', type=int, default=0, required=False)
-parser.add_argument()
+parser.add_argument('-o', '--operations', type=str, default='111', required=False)
 args = parser.parse_args()
 
 file = args.filename + '.txt'
@@ -20,6 +20,15 @@ content = make_split(content)
 print(args.number_of_words)
 print(args.unique_words)
 print(args.duplicates)
+
+if args.operations[0] != '0':
+    print(f'Liczba slow: {no_of_words(content)}')
+if args.operations[1] != '0':
+    print(f'Liczba unikalnych slow: {no_of_unique_elements(content)}')
+if args.operations[2] != '0':
+    print(f'Powtorzenia\n{reps_of_words(content)}')
+
+
 if args.number_of_words == 1:
     print(f'Liczba slow: {no_of_words(content)}')
 if args.unique_words == 1:
