25cba3ae86eaebd2aaf983f06d386a2c5bb1d0d9
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-01-20 11:18:16 +0100

    Object oriented step3

--- Zjazd_5_01_19_24/Gr1_sobota_Jakub/ObjectOrientedProgramming.py
+++ Zjazd_5_01_19_24/Gr1_sobota_Jakub/ObjectOrientedProgramming.py
@@ -5,7 +5,7 @@ class Dog:
         self.owner = owner
 
     def __str__(self):
-        return f"I am Rex, my owner is Tim who says: I think therefore I am."
+        return f"I am {self.name}, my owner is {self.owner} who is {self.owner.age} years old and says: {self.owner.think()}"
 
     def think(self):
         return "Hau hau"
@@ -15,7 +15,7 @@ class Human:
     species = "Homo sapiens"
 
     def think(self):
-        return "I think therefore I am."
+        return "I think therefore I am"
 
 
 class Owner(Human):
@@ -23,13 +23,22 @@ class Owner(Human):
         self.name = name
         self.age = age
 
+    def __str__(self):
+        return self.name
+
 
 tim = Owner("Tim", 10)
+jim = Owner("Jim", 30)
 rex = Dog("Rex", 1, tim)
+dex = Dog("Dex", 3, jim)
 
-print(rex.name)
-print(tim.name)
-print(rex.owner.name)
+print(rex)
+tim.__setattr__("age",11)
+print(dex)
+
+#print(rex.name)
+#print(tim.name)
+#print(rex.owner.name)
 
 """
 Cwiczenie
@@ -38,9 +47,8 @@ Wynik metody think u Rexa
 Wynik metody think u Tima
 Wynik metody think u Tima, odwolujac sie przez bazowy obiekt Rex 
 """
-print(rex.think())
-print(tim.think())
-print(tim.species)
-print(rex.owner.think())
-
-print(rex)
+#print(rex.think())
+#print(tim.think())
+#print(tim.species)
+#print(rex.owner.think())
+#print(rex)
