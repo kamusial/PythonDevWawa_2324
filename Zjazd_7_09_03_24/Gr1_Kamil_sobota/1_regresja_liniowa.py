dbb52515c934e1b88e01b143f7e8ab003194fb29
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-09 10:03:02 +0100

    1

--- Zjazd_7_09_03_24/Gr1_Kamil_sobota/1_regresja_liniowa.py
+++ Zjazd_7_09_03_24/Gr1_Kamil_sobota/1_regresja_liniowa.py
@@ -3,4 +3,26 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 
-pd.read_csv()
+df = pd.read_csv('weight-height.csv')
+print(df)
+print(type(df))
+
+print(df.head(3))   # wyświetl 3 pierwsze wiersze
+print(f'\nzliczanie wartosci kolumny Gender\n{df.Gender.value_counts()}')
+
+#zmiana jednostek
+df.Height *= 2.54
+df.Weight /= 2.2
+print(df.head(3))
+
+#wykresy
+# plt.hist(df.Weight)
+# plt.show()
+
+sns.histplot(df.query("Gender=='Male'").Weight)
+sns.histplot(df.query("Gender=='Female'").Weight)
+plt.show()
+
+# zamiana gender na dane numeryczne
+df = pd.get_dummies(df)
+print(df)
