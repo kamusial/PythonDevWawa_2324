c3a356450688c6e6b199b23ef7b723dbb8413295
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-24 12:03:13 +0100

    1

--- Zjazd8_23_03_24/Gr1_Kamil_Neuronowe/Niedziela_Tkinter/10_przyciski.py
+++ Zjazd8_23_03_24/Gr1_Kamil_Neuronowe/Niedziela_Tkinter/10_przyciski.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 root = tk.Tk()
-
-root.geometry('500x600')
+root.attributes('-topmost', 1)
+root.geometry('500x600+1400+50')
 root.title('taki program')
 
 label = tk.Label(root, text='Hello', font=('Arial', 24))
@@ -11,22 +11,22 @@ textbox.pack(padx=10, pady=20)
 
 buttonframe = tk.Frame(root)
 buttonframe.columnconfigure(0, weight=1)
-buttonframe.columnconfigure(1, weight=1)
+buttonframe.columnconfigure(1, weight=2)
 buttonframe.columnconfigure(2, weight=1)
 btn1 = tk.Button(buttonframe, text='1', font=('Arial', 15))
-btn1.grid(row=0, column=0)
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E, padx=5, pady=5)
 btn2 = tk.Button(buttonframe, text='2', font=('Arial', 15))
-btn2.grid(row=0, column=1)
+btn2.grid(row=0, column=1, sticky=tk.W+tk.E, padx=5, pady=5)
 btn3 = tk.Button(buttonframe, text='3', font=('Arial', 15))
-btn3.grid(row=0, column=2)
+btn3.grid(row=0, column=2, sticky=tk.W+tk.E, padx=5, pady=5)
 btn4 = tk.Button(buttonframe, text='4', font=('Arial', 15))
-btn4.grid(row=1, column=0)
+btn4.grid(row=1, column=0, sticky=tk.W+tk.E, padx=5, pady=5)
 btn5 = tk.Button(buttonframe, text='5', font=('Arial', 15))
-btn5.grid(row=1, column=1)
+btn5.grid(row=1, column=1, sticky=tk.W+tk.E, padx=5, pady=5)
 btn6 = tk.Button(buttonframe, text='6', font=('Arial', 15))
-btn6.grid(row=1, column=2)
+btn6.grid(row=1, column=2, sticky=tk.W+tk.E, padx=5, pady=5)
+buttonframe.pack(fill='x')
 
-buttonframe.pack()
-
-
+nextbtn = tk.Button(root, text='TEST')
+nextbtn.place(x=200, y=200, height=100, width=100)
 root.mainloop()
