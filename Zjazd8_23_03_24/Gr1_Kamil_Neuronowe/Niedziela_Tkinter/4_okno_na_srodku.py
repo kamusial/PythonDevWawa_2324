9bcccba813746c6a497333649a8eaea5e0211a15
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-24 09:30:23 +0100

    1

--- Zjazd8_23_03_24/Gr1_Kamil_Neuronowe/Niedziela_Tkinter/4_okno_na_srodku.py
+++ Zjazd8_23_03_24/Gr1_Kamil_Neuronowe/Niedziela_Tkinter/4_okno_na_srodku.py
@@ -9,5 +9,10 @@ screen_height = root.winfo_screenheight()
 print(screen_height, screen_width)
 
 # otwórz okno na środku
+center_x = int(screen_width/2 - window_width / 2)
+center_y = int(screen_height/2 - window_height / 2)
+root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+root.mainloop()
+
 
 root.mainloop()
---
+++ Zjazd8_23_03_24/Gr1_Kamil_Neuronowe/Niedziela_Tkinter/5_staly_rozmiar_okna.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('zablokowanie zmiany rozmaru okna')
+root.geometry('600x400+1200+50')
+
+root.mainloop()
