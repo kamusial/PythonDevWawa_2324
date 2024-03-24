e705fd1c856658b587c8e10167c08f7f47608e6d
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-24 08:41:18 +0100

    1

--- Zjazd8_23_03_24/Gr1_Kamil_Neuronowe/3_ANN.py
+++ Zjazd8_23_03_24/Gr1_Kamil_Neuronowe/3_ANN.py
@@ -9,7 +9,20 @@ from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 
+######### Pandas settings
+# pd.set_option('display.max_rows', 5000)
+pd.set_option('display.max_columns', 5000)
+pd.set_option('display.width', 10000)
+pd.set_option('display.max_colwidth', 10000)
+np.set_printoptions(linewidth=2000)
+
 X, y = load_breast_cancer(return_X_y=True, as_frame=True)
+
+print(X)
+print(X.shape)
+print(X.columns)
+print(X.describe())
+
 
 ### wersja 1 ###
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
@@ -20,17 +33,17 @@ X, y = load_breast_cancer(return_X_y=True, as_frame=True)
 # print(accuracy_score(y_test, y_pred))
 
 ### wersja 2 ###
-score = []
-kfold = RepeatedStratifiedKFold(n_splits=10, n_repeats=30)
-for train, test in kfold.split(X,y):
-    model = MLPClassifier(hidden_layer_sizes=(10, 10, 10, 10, 10), max_iter=5000, activation='relu')
-    X_train, X_test = X.iloc[train,:], X.iloc[test,:]
-    y_train, y_test = y.iloc[train], y.iloc[test]
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    score.append(accuracy_score(y_test, y_pred))
-print(score)
-
-plt.plot(score)
-plt.grid()
-plt.show()
+# score = []
+# kfold = RepeatedStratifiedKFold(n_splits=10, n_repeats=30)
+# for train, test in kfold.split(X,y):
+#     model = MLPClassifier(hidden_layer_sizes=(10, 10, 10, 10, 10), max_iter=5000, activation='relu')
+#     X_train, X_test = X.iloc[train,:], X.iloc[test,:]
+#     y_train, y_test = y.iloc[train], y.iloc[test]
+#     model.fit(X_train, y_train)
+#     y_pred = model.predict(X_test)
+#     score.append(accuracy_score(y_test, y_pred))
+# print(score)
+#
+# plt.plot(score)
+# plt.grid()
+# plt.show()
