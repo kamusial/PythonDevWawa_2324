c4742d3a96b47dae598a7fa2a22230305cccc7cf
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-03-24 09:44:22 +0100

    1

--- Zjazd8_23_03_24/Gr1_Kamil_Neuronowe/Niedziela_Tkinter/5_staly_rozmiar_okna.py
+++ Zjazd8_23_03_24/Gr1_Kamil_Neuronowe/Niedziela_Tkinter/5_staly_rozmiar_okna.py
@@ -3,5 +3,10 @@ import tkinter as tk
 root = tk.Tk()
 root.title('zablokowanie zmiany rozmaru okna')
 root.geometry('600x400+1200+50')
+#root.resizable(False, True)
+root.minsize(300, 300)
+root.maxsize(700, 500)
+root.attributes('-alpha', 0.8)   # przeźroczystość
+root.attributes('-topmost', 1)   # na wierzchu
 
 root.mainloop()
