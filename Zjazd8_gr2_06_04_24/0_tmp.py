da5c34d7b5c23f6aa149fcf4b556795785e2da18
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-04-06 10:00:43 +0200

    1

---
+++ Zjazd8_gr2_06_04_24/0_tmp.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv('weight-height.csv', delimiter=';')
+print(df)
+print(type(df))
+
+df.Height *= 2.54
+df.Weight /= 2.2
+print(df.head(3)) #pierwsze 3 wiersze
+
+# # wykresy
+# plt.hist(df.query('Gender=="Male"').Weight)
+# #plt.show()
+# plt.hist(df.query('Gender=="Female"').Weight)
+# plt.show()
+#
+# sns.histplot(df.query('Gender=="Male"').Weight)
+# sns.histplot(df.query('Gender=="Female"').Weight)
+# plt.show()
+
+# zamiana gender na dane numeryczne
+df = pd.get_dummies(df)
+del (df['Gender_Male'])
+df = df.rename(columns={'Gender_Female': 'Gender'})
+print(df.head())
