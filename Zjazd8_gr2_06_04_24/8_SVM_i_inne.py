6bdd52c43ff9180a534b8b9fd263644b5eee38cc
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-04-07 11:59:42 +0200

    1

--- Zjazd8_gr2_06_04_24/8_SVM_i_inne.py
+++ Zjazd8_gr2_06_04_24/8_SVM_i_inne.py
@@ -1,12 +1,13 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_circles
+from sklearn.svm import SVC  #clasifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
-from sklearn.svm import SVC
-from sklearn.datasets import make_circles
+
 X, y = make_circles(n_samples=5000, factor=0.4, noise=0.2)
 print(X)
 print(y)
@@ -18,8 +19,26 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
 # kernel{‘linear’, ‘poly’, ‘rbf’, ‘sigmoid’, ‘precomputed’}
 
 
+print('\nLogistic Regression')
+model = LogisticRegression()
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nKNN')
+model = KNeighborsClassifier(n_neighbors=50, weights='distance')
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
+print('\nDrzewo decyzyjne')
+model = DecisionTreeClassifier(max_depth=3, min_samples_split=5)
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+
 print('\nSVC')
-model = SVC(kernel='rbf', degree=2)
+model = SVC()
 model.fit(X_train, y_train)
 print(model.score(X_test, y_test))
 print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
