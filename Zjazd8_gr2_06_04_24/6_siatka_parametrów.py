7ac8f7c3937dbf40336d2c387daa675bc5260ce8
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-04-07 09:51:10 +0200

    1

---
+++ Zjazd8_gr2_06_04_24/6_siatka_parametrów.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+
+df = pd.read_csv('heart.csv', comment='#')
+print(df.head().to_string())
+
+X = df.iloc[:, :-1]
+y = df.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = DecisionTreeClassifier(criterion='gini', max_depth=6, min_samples_split=2)
+model.fit(X_train, y_train)
+print(model.score(X_test, y_test))
+print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
+print(pd.DataFrame(model.feature_importances_, X.columns))
+
