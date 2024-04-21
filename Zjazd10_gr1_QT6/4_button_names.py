64d6b97ad49972a3e71cffbf28cb8c2a3127fca6
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-04-21 09:54:05 +0200

    1

--- Zjazd10_gr1_QT6/4_button_names.py
+++ Zjazd10_gr1_QT6/4_button_names.py
@@ -14,3 +14,33 @@ tytuly_okien = [
     'Ania',
     'BLAD!'
 ]
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Moja aplikacja')
+        self.setFixedSize(QSize(600, 400))
+        self.button = QPushButton('przycisk')
+        self.button.clicked.connect(self.button_clicked)
+        self.windowTitleChanged.connect(self.wrong_title)
+
+        self.setCentralWidget(self.button)
+
+    def button_clicked(self):
+        print('klikniety')
+        tytul = choice(tytuly_okien)
+        print('nowy tytul to: ',tytul)
+        self.setWindowTitle(tytul)
+
+    def wrong_title(self, tytul_okna):
+        print('Tytul zmieniono na ',tytul_okna)
+        if tytul_okna == 'BLAD!':
+            self.button.setText('Nieaktywny')
+            self.button.setDisabled(True)
+
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
