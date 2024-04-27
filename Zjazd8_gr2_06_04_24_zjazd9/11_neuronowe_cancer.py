ff69fcf06ddd3f194996aeca35fc60199279e59d
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-04-27 11:12:03 +0200

    1

--- Zjazd8_gr2_06_04_24_zjazd9/11_neuronowe_cancer.py
+++ Zjazd8_gr2_06_04_24_zjazd9/11_neuronowe_cancer.py
@@ -21,3 +21,34 @@ from tensorflow.keras.datasets import mnist, cifar10
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+pd.set_option('display.max_rows', 5000)
+pd.set_option('display.max_columns', 5000)
+pd.set_option('display.width', 10000)
+pd.set_option('display.max_colwidth', 10000)
+np.set_printoptions(linewidth=2000)
+
+X, y = load_breast_cancer(return_X_y=True, as_frame=True)
+print(X.head())
+print(X.shape)
+print(X.describe())
+print(Counter(y))
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+# model = MLPClassifier(max_iter=1000, hidden_layer_sizes=(10, 10, 10))
+# model.fit(X_train, y_train)
+# y_pred = model.predict(X_test)
+# print(accuracy_score(y_test, y_pred))
+
+score = []
+kfold = RepeatedStratifiedKFold(n_splits=10, n_repeats=1)
+for train, test in kfold.split(X, y):
+    model = MLPClassifier(max_iter=1000, hidden_layer_sizes=(100))
+    X_train, X_test = X.iloc[train, :], X.iloc[test,:]
+    y_train, y_test = y.iloc[train], y.iloc[test]
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    score.append(accuracy_score(y_test, y_pred))
+print(score)
+plt.plot(score)
+plt.show()
