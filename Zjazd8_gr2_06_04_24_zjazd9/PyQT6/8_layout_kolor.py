deb79d2cfac5d77cc58fca19fecb384a6a801900
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-04-28 13:05:22 +0200

    1

--- Zjazd8_gr2_06_04_24_zjazd9/PyQT6/8_layout_kolor.py
+++ Zjazd8_gr2_06_04_24_zjazd9/PyQT6/8_layout_kolor.py
@@ -7,7 +7,22 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setWindowTitle('Kolory')
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(Color('red'))
+        self.layout.addWidget(Color('green'))
+        self.layout.addWidget(Color('blue'))
 
+        self.widget = QWidget()
+        self.widget.setLayout(self.layout)
+        self.setCentralWidget(self.widget)
+
+class Color(QWidget):
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
 
 
 app = QApplication(sys.argv)
