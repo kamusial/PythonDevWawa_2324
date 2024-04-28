74185c9dbf6245c07f01b39ca1390a468cc67526
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-04-28 09:04:30 +0200

    1

--- Zjazd8_gr2_06_04_24_zjazd9/Tkinter/OOP.py
+++ Zjazd8_gr2_06_04_24_zjazd9/Tkinter/OOP.py
@@ -6,7 +6,7 @@ class MyGui:
         self.root = tk.Tk()
         self.root.title('Program GUI')
         self.root.geometry('600x400+1200+80')
-        self.root.attributes('-alpha', 0.5)
+        self.root.attributes('-alpha', 0.8)
         self.root.attributes('-topmost', 1)
         self.root.config(background='Light Blue')
 
@@ -23,11 +23,24 @@ class MyGui:
         self.textbox.bind('<KeyPress>', self.short)
         self.textbox.pack(padx=10, pady=10)
 
+        self.check_state = tk.IntVar()
+        self.check = tk.Checkbutton(self.root, text='show', font=('Arial', 15), command=self.show_message)
+        self.check.pack(padx=10, pady=10)
+
         self.root.mainloop()
+
+    def show_message(self):
+        print('Teraz show message')
+        print(self.check_state.get())
+        if self.check_state.get() == 0:
+            print(self.textbox.get('1.0', tk.END))
+        else:
+            messagebox.showinfo(title='Message', message=self.textbox.get('1.0', tk.END))
 
     def short(self, event):
         print(event.keysym)
         print(event.state)
         if event.keysym == 'Return' and event.state == 12:
             print('Nacisnales enter + ctrl')
+            self.show_message()
 MyGui()
