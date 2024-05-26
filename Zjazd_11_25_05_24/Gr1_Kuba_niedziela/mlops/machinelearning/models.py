f76eba1b7e3dcb412f7e1973aeb3a1b45b9856d9
Runhorn <jakub.kurcwald@gmail.com>
2024-05-26 11:15:11 +0200

    Start of a model

--- Zjazd_11_25_05_24/Gr1_Kuba_niedziela/mlops/machinelearning/models.py
+++ Zjazd_11_25_05_24/Gr1_Kuba_niedziela/mlops/machinelearning/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import datetime
+import joblib
 # Create your models here.
+
+class MLModel(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    upload = models.FileField()
+    accuracy = models.DecimalField(decimal_places=3, max_digits=5)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    published = models.DateTimeField(default=datetime.now)
