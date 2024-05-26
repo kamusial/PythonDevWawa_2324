31982791d5c8a7cbb4edb197107c1e8ea38a0eaa
Runhorn <jakub.kurcwald@gmail.com>
2024-05-26 09:32:19 +0200

    Model training as a function

--- Zjazd_11_25_05_24/Gr1_Kuba_niedziela/machine_learning_models/model.py
+++ Zjazd_11_25_05_24/Gr1_Kuba_niedziela/machine_learning_models/model.py
@@ -1,24 +1,27 @@
-import pandas as pd
+import random
+
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_circles
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
-
-X , y = make_circles(n_samples=500, factor=0.3, noise=0.4)
-plt.scatter(X[:, 0], X[:, 1], c=y)
-plt.show()
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
-
-print('\nLogistic Regression')
-model = LogisticRegression()
-model.fit(X_train, y_train)
-print(model.score(X_test, y_test))
-print(pd.DataFrame(confusion_matrix(y_test, model.predict(X_test))))
-
 import joblib
-joblib.dump(model, 'my_model')
 
-new_model = joblib.load('my_model')
-print(new_model.predict([[1, 1]]))
+
+def train_model(n_samples, factor, noise, test_size):
+    X , y = make_circles(n_samples=n_samples, factor=factor, noise=noise)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    score = model.score(X_test, y_test)
+    print(f'Got model with score: {score}.')
+    if score > 0.45:
+        model_name = 'my_model_' + str(random.randint(0,1000))
+        joblib.dump(model, model_name)
+        print(f'Saved model: {model_name}')
+    else:
+        print('Model was not good enough!')
+
+for i in range(10):
+    train_model(1000, 0.9, 0.1, 0.2)
+
+random.randint(0,99) * 0.01
