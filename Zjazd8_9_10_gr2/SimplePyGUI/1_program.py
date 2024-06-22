5c4d294def4686752359063ba4b47406f25f9146
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-06-22 14:29:25 +0200

    1

--- Zjazd8_9_10_gr2/SimplePyGUI/1_program.py
+++ Zjazd8_9_10_gr2/SimplePyGUI/1_program.py
@@ -8,10 +8,23 @@ def convert_to_csv(filepath, output_folder, sheet, separator, decimal):
     outputfile = Path(output_folder) / f'{filename}.csv'
     df.to_csv(outputfile, sep=separator, decimal=decimal, index=False)
 
+
+def pokaz(filepath, sheet):
+    df = pd.read_excel(filepath, sheet)
+    filename = Path(filepath).stem
+    sg.popup_scrolled(df.dtypes, '-' * 20, df.to_string(), title=filename)
+
+
+def dobra_sciezka(filepath):
+    if filepath and Path(filepath).exists():
+        return True
+    sg.popup_error("nie ma sciezki")
+    return False
+
 layout = [
     [sg.Text("Plik wejsciowy"), sg.Input(key='wejscie'), sg.FileBrowse(file_types=(('Pliki excel', '*.xls*'),))],
     [sg.Text("Folder wYjsciowy"), sg.Input(key='wyjscie'), sg.FolderBrowse()],
-    [sg.Button("konwertuj"), sg.Button("Wyjdz"), sg.Exit(button_text='EXXXXIT', button_color='blue')]
+    [sg.Button("konwertuj"), sg.Button("pokaz"), sg.Exit(button_text='EXXXXIT', button_color='blue')]
 
 ]
 
@@ -20,7 +33,7 @@ window = sg.Window("Moj program konversja do csv", layout)
 while True:
     event, values = window.read()
     print(event, values)
-    if event in (sg.WIN_CLOSED, "Wyjdz", "EXXXXIT"):
+    if event in (sg.WIN_CLOSED, "EXXXXIT"):
         break
     if event == "konwertuj":
         convert_to_csv(
@@ -30,6 +43,9 @@ while True:
             separator='|',
             decimal='.'
         )
+    if event == 'pokaz':
+        if dobra_sciezka(filepath=values['wejscie']):
+            pokaz(filepath=values['wejscie'],sheet='Call Center Data')
 
 window.close()
 
