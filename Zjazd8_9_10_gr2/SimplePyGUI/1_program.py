7efa99b4b7957ba9d95196fedb949760bdfc54e1
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-06-22 14:13:52 +0200

    1

--- Zjazd8_9_10_gr2/SimplePyGUI/1_program.py
+++ Zjazd8_9_10_gr2/SimplePyGUI/1_program.py
@@ -6,24 +6,30 @@ def convert_to_csv(filepath, output_folder, sheet, separator, decimal):
     df = pd.read_excel(filepath, sheet)
     filename = Path(filepath).stem
     outputfile = Path(output_folder) / f'{filename}.csv'
-    df.to_csv(outputfile, sep=separator, decimal=decimal)
+    df.to_csv(outputfile, sep=separator, decimal=decimal, index=False)
 
 layout = [
-    [sg.Text("Pole 1ererererere"), sg.Input(), sg.FileBrowse()],
-    [sg.Text("Pole 2         "), sg.Input(), sg.FolderBrowse()],
-    [sg.Button("policz srednia"), sg.Button("Wyjdz")]
+    [sg.Text("Plik wejsciowy"), sg.Input(key='wejscie'), sg.FileBrowse(file_types=(('Pliki excel', '*.xls*'),))],
+    [sg.Text("Folder wYjsciowy"), sg.Input(key='wyjscie'), sg.FolderBrowse()],
+    [sg.Button("konwertuj"), sg.Button("Wyjdz"), sg.Exit(button_text='EXXXXIT', button_color='blue')]
 
 ]
 
-window = sg.Window("Moj program", layout)
+window = sg.Window("Moj program konversja do csv", layout)
 
 while True:
     event, values = window.read()
     print(event, values)
-    if event in (sg.WIN_CLOSED, "Wyjdz"):
+    if event in (sg.WIN_CLOSED, "Wyjdz", "EXXXXIT"):
         break
-    if event == "policz srednia":
-        sg.popup_error('Funkcja niezaimplemetowana')
+    if event == "konwertuj":
+        convert_to_csv(
+            filepath=values['wejscie'],
+            output_folder=values['wyjscie'],
+            sheet='Call Center Data',
+            separator='|',
+            decimal='.'
+        )
 
 window.close()
 
